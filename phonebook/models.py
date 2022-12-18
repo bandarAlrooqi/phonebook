fa2aadc1be311545ceb89e2597e5da715d97c430
Bandar Alrooqi <officialAlrooqi@gmail.com>
2022-12-18 12:42:22 +0300

    add file

--- phonebook/models.py
+++ phonebook/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Number(models.Model):
+    # store phone number
+    number = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.number)
+
+
+# create a model for the phonebook
+class Phonebook(models.Model):
+    # store name
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    # store phone number
+    number = models.ForeignKey(Number, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
--- phonebook/views.py
+++ phonebook/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from phonebook.models import *
+
+
 # Create your views here.
+def home(request):
+    contacts = Phonebook.objects.select_related('number').all()
+    return render(request, 'index.html', {'contacts': contacts})
