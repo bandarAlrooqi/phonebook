961716272e85bdfa7fe45f486d2707fbe50dbbbc
Bandar Alrooqi <officialAlrooqi@gmail.com>
2022-12-18 15:51:32 +0300

    finalize

--- phonebook/form.py
+++ phonebook/form.py
@@ -7,9 +7,12 @@ class Contact(forms.ModelForm):
     class Meta:
         model = Phonebook
         fields = ['first_name', 'last_name']
-
-        # add list of numbers to the form
+        # make all fields required
+        widgets = {
+            'first_name': forms.TextInput(attrs={'required': True}),
+            'last_name': forms.TextInput(attrs={'required': True}),
+        }
 
 
 class Contact_number(forms.Form):
-    number = forms.CharField(max_length=10, min_length=10)
+    number = forms.CharField(max_length=10, min_length=10, required=True)
