c719450ce66246e67b726f24d9559c655584d1d8
Bandar Alrooqi <officialAlrooqi@gmail.com>
2022-12-18 14:38:09 +0300

    finalize

--- phonebook/form.py
+++ phonebook/form.py
@@ -4,16 +4,12 @@ from .models import *
 
 class Contact(forms.ModelForm):
     # allow user to enter only 11 digits
-    number = forms.CharField(max_length=11, min_length=11, widget=forms.TextInput(attrs={'class': 'form-control'}))
-
     class Meta:
         model = Phonebook
-        fields = ['first_name', 'last_name', 'number']
+        fields = ['first_name', 'last_name']
 
         # add list of numbers to the form
 
 
-class Contact_number(forms.ModelForm):
-    class Meta:
-        model = Number
-        fields = ['number']
+class Contact_number(forms.Form):
+    number = forms.CharField(max_length=10, min_length=10)
--- phonebook/views.py
+++ phonebook/views.py
@@ -1,27 +1,31 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-from phonebook.models import *
+from django.forms import formset_factory
 from .form import *
 
 
 # Create your views here.
 def home(request):
-    # query the database for all numbers and send only unique name to the template
     contacts = Phonebook.objects.all()
     return render(request, 'index.html', {'contacts': contacts})
 
 
 def add(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        number = request.POST['number']
-        Phonebook.objects.create(first_name=first_name, last_name=last_name,
-                                 number=Number.objects.create(number=number))
-    context = {
-        'form': Contact(),
-    }
-    return render(request, 'add.html', context)
+        form = Contact(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # get all numbers from the form
+            numbers = request.POST.dict()
+            # remove the csrf token and the name from the dictionary
+            numbers.pop('csrfmiddlewaretoken')
+            numbers.pop('first_name')
+            numbers.pop('last_name')
+            # loop through the dictionary and save the numbers
+            for number in numbers.values():
+                Number.objects.create(number=number, phonebook=user)
+            return HttpResponseRedirect('/display/' + str(user.id))
+    return render(request, 'add.html', {'form': Contact(), 'formset':formset_factory(Contact_number)})
 
 
 def display(request, pk):
